47ce10b659b58bb14ada0962983a8685f3d9c9ac
jdcraven <jdcraven@email.neit.edu>
2015-05-31 01:58:23 -0400

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

--- Final/TimeManagement/site/mvc/views/signup.php
+++ Final/TimeManagement/site/mvc/views/signup.php
@@ -30,24 +30,26 @@
              if ( isset($scope->view['errors']) ) {
                 print_r($scope->view['errors']);
              }
-             
-              if ( $signupDao->create($model) ) {
-                    echo '<h2>Signup complete</h2>';
-                } else {
-                    echo '<h2>Signup Failed</h2>';
-                }
+             if ( isset($scope->view['saved']) && $scope->view['saved'] ) {
+                  echo 'Signup Added';
+             }
+//              if ( $signupDao->create($model) ) {
+//                    echo '<h2>Signup complete</h2>';
+//                } else {
+//                    echo '<h2>Signup Failed</h2>';
+//                }
              
          }
             
         $email = $scope->view['model']->getEmail();
-            
+        $password = $scope->view['model']->getPassword();    
         ?>
         
         <h1>Signup</h1>
         <form action="#" method="POST">
             
             Email : <input type="email" name="email" value="<?php echo $email; ?>" placeholder=" Please Enter a valid Email"/> <br />
-            Password : <input type="password" name="password" value="" /> <br /> 
+            Password : <input type="password" name="password" value="<?php echo$password; ?>" /> <br /> 
             <br />
             <input type="submit" value="Signup" />
             
